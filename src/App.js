1365a557fd320adec9ea2c033b914d7db6a445c9
victorloo <ila9970@gmail.com>
2018-09-23 03:37:28 +0800

    Lists & State

--- src/App.js
+++ src/App.js
@@ -32,6 +32,12 @@ class App extends Component {
     });
   }
 
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+  }
+
   togglePersonsHandler = () => {
     const doseShow = this.state.showPersons;
     this.setState({showPersons: !doseShow});
@@ -51,9 +57,10 @@ class App extends Component {
     if (this.state.showPersons === true) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
+          {this.state.persons.map((person, index) => {
             return (
               <Person
+                click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age} />
             )
