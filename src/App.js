ecdf4542508954e61f33de2ebd2c4f84cd1d2dc6
victorloo <ila9970@gmail.com>
2018-09-23 03:06:48 +0800

    Passing Method References by executing arrow function

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!!</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Mr. Garrison')} >Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Mr. Garrison')} >Switch Name</button>
         <Person 
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
