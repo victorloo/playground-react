36643d0903880d18db2b9caef81e37ed732a0fde
victorloo <ila9970@gmail.com>
2018-09-23 03:50:35 +0800

    Flexible Lists

--- src/App.js
+++ src/App.js
@@ -22,14 +22,23 @@ class App extends Component {
     })
   }
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'David', age: 30 },
-        { name: event.target.value, age: 32 },
-        { name: 'Jaina', age: 40 }
-      ]
-    });
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -64,7 +73,8 @@ class App extends Component {
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age} 
-                key={person.id} />
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)} />
             )
           })}
         </div>
