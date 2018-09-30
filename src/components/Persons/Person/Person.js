45d46b5ce89881c632c319116136ba78c17e8e30
victorloo <ila9970@gmail.com>
2018-10-01 02:07:26 +0800

    Using React 16.3 sytax to ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -8,6 +8,7 @@ class Person extends Component {
   constructor( props ) {
     super(props);
     console.log('[Person.js] Inside Constructor()', props);
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -17,7 +18,7 @@ class Person extends Component {
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()');
     if (this.props.position === 0) {
-      this.inputElement.focus(); 
+      this.inputElement.current.focus(); 
     }
   }
 
@@ -32,7 +33,7 @@ class Person extends Component {
         <p onClick={this.props.click} >I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input
-          ref={(inp) => { this.inputElement = inp }}
+          ref={this.inputElement}
           type="text"
           onChange={this.props.changed}
           value={this.props.name} />
