5f795bd43fdbe357674e88cbe3ecd7589eb2fd8f
victorloo <ila9970@gmail.com>
2018-09-30 23:37:32 +0800

    Using References("ref")

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -16,6 +16,9 @@ class Person extends Component {
 
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()');
+    if (this.props.position === 0) {
+      this.inputElement.focus(); 
+    }
   }
 
   componentWillUnmount() {
@@ -28,7 +31,11 @@ class Person extends Component {
       <React.Fragment>
         <p onClick={this.props.click} >I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
+        <input
+          ref={(inp) => { this.inputElement = inp }}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name} />
       </React.Fragment>
     )
   }
