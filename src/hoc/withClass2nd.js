5282792f70a9bf9aff1284989da2dcbbe6c07162
victorloo <ila9970@gmail.com>
2018-09-30 22:49:11 +0800

    Use stateful component in HOC

--- src/hoc/withClass2nd.js
+++ src/hoc/withClass2nd.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+/*
 const withClass2 = (WrappedComponent, className) => {
   return (props) => (
     <div className={className}>
       <WrappedComponent {...props}/>
     </div>
   )
+}
+*/
+
+const withClass2 = (WrappedComponent, className) => {
+  return class extends Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />
+        </div>
+      )
+    }
+  }
 }
 
 export default withClass2;
